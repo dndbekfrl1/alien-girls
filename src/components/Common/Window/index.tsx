23495d5d9847a4dd1802574628a4a350040abc06
dndbekfrl1 <dndbekfrl1@gmail.com>
2022-10-02 16:25:21 +0900

    Update application draggable
    - react-dragabble 적용

--- src/components/Common/Window/index.tsx
+++ src/components/Common/Window/index.tsx
@@ -1,27 +1,45 @@
+import styled from 'styled-components';
+import Draggable from 'react-draggable';
+
 interface Props {
   open?: boolean;
   title?: string;
   TitleBar?: React.ReactElement;
   style?: React.CSSProperties;
+  defaultX?: number;
+  defaultY?: number;
   children?: React.ReactElement;
   onClose?: any;
 }
 
-function Window({ open, title, TitleBar, children, style, onClose }: Props) {
+const Layout = styled.div``;
+
+function Window({
+  open,
+  title,
+  TitleBar,
+  children,
+  style,
+  defaultX,
+  defaultY,
+  onClose,
+}: Props) {
   if (!open) return <></>;
 
   return (
-    <div style={style} className='window'>
-      <div className='title-bar'>
-        <div className='title-bar-text'>{title}</div>
-        <div className='title-bar-controls'>
-          <button aria-label='Minimize' />
-          <button aria-label='Maximize' />
-          <button aria-label='Close' onClick={onClose} />
+    <Draggable defaultPosition={{ x: defaultX || 0, y: defaultY || 0 }}>
+      <Layout style={style} className='window'>
+        <div className='title-bar'>
+          <div className='title-bar-text'>{title}</div>
+          <div className='title-bar-controls'>
+            <button aria-label='Minimize' />
+            <button aria-label='Maximize' />
+            <button aria-label='Close' onClick={onClose} />
+          </div>
         </div>
-      </div>
-      <div className='window-body'>{children}</div>
-    </div>
+        <div className='window-body'>{children}</div>
+      </Layout>
+    </Draggable>
   );
 }
 
