dbb919f3c06dfb5807987ebba248bde83c462903
dndbekfrl1 <dndbekfrl1@gmail.com>
2022-10-03 20:31:42 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Layout from 'components/Common/Layout.index';
+import TaskBar from 'components/Common/TaskBar';
+import Desktop from 'components/Desktop';
+import { RecoilRoot } from 'recoil';
+
 function App() {
-  return <div>hello world!</div>;
+  return (
+    <RecoilRoot>
+      <Layout>
+        <>
+          <Desktop />
+          <TaskBar />
+        </>
+      </Layout>
+    </RecoilRoot>
+  );
 }
 
 export default App;
