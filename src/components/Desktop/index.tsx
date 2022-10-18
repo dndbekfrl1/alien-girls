20369de31cb1b6febb7fa7cb812dd84a986744c5
dndbekfrl1 <dndbekfrl1@gmail.com>
2022-10-18 22:45:56 +0900

    #2 Add artworks

--- src/components/Desktop/index.tsx
+++ src/components/Desktop/index.tsx
@@ -5,6 +5,9 @@ import {
   Center,
   QuizLayout,
 } from 'components/Desktop/Desktop.styled';
+import { AppEnum } from 'types/app';
+import Memo from 'components/Memo';
+import Quiz from 'components/Quiz';
 import folder from 'assets/icons/folder.png';
 import internet from 'assets/icons/internet.png';
 import memo from 'assets/icons/memo.png';
@@ -15,10 +18,10 @@ import artworkA from 'assets/images/artworka.png';
 import artworkI from 'assets/images/artworki.jpeg';
 import artworkL from 'assets/images/artworkl.jpeg';
 import artworkE from 'assets/images/artworke.png';
+import artwork5 from 'assets/images/artwork5.png';
+import artwork6 from 'assets/images/artwork6.png';
+import artwork7 from 'assets/images/artwork7.png';
 import internetExplorer from 'assets/images/internet-explorer.gif';
-import { AppEnum } from 'types/app';
-import Memo from 'components/Memo';
-import Quiz from 'components/Quiz';
 
 function Desktop() {
   return (
@@ -68,47 +71,54 @@ const ALIENGIRLS = [
   {
     id: AppEnum.A1,
     name: 'A',
-    defaultX: 745,
-    defaultY: 215,
+    defaultX: 511,
+    defaultY: -573,
     src: artworkA,
     style: { width: '1666px', height: '1838px' },
   },
   {
     id: AppEnum.L2,
     name: 'L',
-    defaultX: 325,
-    defaultY: 1450,
+    defaultX: 175,
+    defaultY: 781,
     src: artworkL,
     style: { width: '1195px', height: '990px' },
   },
   {
     id: AppEnum.I3,
     name: 'I',
-    defaultX: 2837,
-    defaultY: 448,
+    defaultX: 2665,
+    defaultY: -434,
     src: artworkI,
     style: { width: '1389', height: '990px' },
   },
   {
     id: AppEnum.E4,
     name: 'E',
-    defaultX: 3520,
-    defaultY: 1860,
+    defaultX: 3313,
+    defaultY: 572,
     src: artworkE,
     style: { width: '1384px', height: '1123px' },
   },
   {
     id: AppEnum.N5,
     name: 'N',
+    defaultX: 1588,
+    defaultY: 1072,
+    src: artwork5,
   },
 
   {
     id: AppEnum.G6,
     name: 'G',
+    defaultX: 2442,
+    defaultY: 439,
+    src: artwork6,
   },
   {
     id: AppEnum.I7,
     name: 'I',
+    src: artwork7,
   },
   {
     id: AppEnum.R8,
