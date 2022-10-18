53de3e912390e3cd424322cf3a72862eee01fc57
dndbekfrl1 <dndbekfrl1@gmail.com>
2022-10-18 23:57:56 +0900

    #2 Delete file
    - 사용하지 않는 유틸 함수 삭제

--- src/utils/calculatePercentage.ts
+++
@@ -1,7 +0,0 @@
-function calculatePercentage(
-  px: number,
-  direction: 'width' | 'height' = 'width'
-) {
-  return direction === 'width' ? px / 5120 : px / 2880;
-}
-export default calculatePercentage;
